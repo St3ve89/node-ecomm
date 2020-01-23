33a0faebc4f0541f49f09711c39c9e3f7379f678
Istvan Acs <istvan.acs89@gmail.com>
2020-01-23 22:01:59 +0000

    Create a basics web server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('huhu');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hi there');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
