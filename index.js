63108261724872c8adcc9ecd7085de90b6db2306
Istvan Acs <istvan.acs89@gmail.com>
2020-01-25 13:11:34 +0000

    Middlewares in Express

--- index.js
+++ index.js
@@ -15,17 +15,26 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', (req, res) => {
+const bodyParser = (req, res, next) => {
+  if (req.method === 'POST') {
+    req.on('data', data => {
+      const parsed = data.toString('utf8').split('&');
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=');
+        formData[key] = value;
+      }
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post('/', bodyParser, (req, res) => {
   // get access to email, password, password confirmation
-  req.on('data', data => {
-    const parsed = data.toString('utf8').split('&');
-    const formData = {};
-    for (let pair of parsed) {
-      const [key, value] = pair.split('=');
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
+  console.log(req.body);
   res.send(`Account created!!! `);
 });
 
