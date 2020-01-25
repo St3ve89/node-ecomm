147bbbecab1fa20c3a5cf5c1e4773bdbddc65f80
Istvan Acs <istvan.acs89@gmail.com>
2020-01-25 12:25:02 +0000

    Understanding form submission

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`
     <div>
-      <form>
-        <input placeholder="email"/>
-        <input placeholder="password"/>
-        <input placeholder="password confirmation"/>
+      <form method='POST'>
+        <input name='email' placeholder="email"/>
+        <input name='password' placeholder="password"/>
+        <input name='psaswordConfirmation' placeholder="password confirmation"/>
         <button>Sign Up</button>
       </form>
     </div>
   `);
+});
+
+app.post('/', (req, res) => {
+  res.send(`Account created!!! `);
 });
 
 const PORT = 3000;
