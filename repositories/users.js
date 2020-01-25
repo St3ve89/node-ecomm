ac0f8ca2845f8dc840301ecdd2968214f83e7413
Istvan Acs <istvan.acs89@gmail.com>
2020-01-25 14:35:07 +0000

    Finding by id

--- repositories/users.js
+++ repositories/users.js
@@ -47,16 +47,24 @@ class UsersRepository {
   randomId() {
     return crypto.randomBytes(4).toString('hex');
   }
+
+  async getOne(id) {
+    const records = await this.getAll();
+
+    return records.find(record => record.id === id);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  await repo.create({ email: 'test@test.com', password: 'password' });
+  // await repo.create({ email: 'test@test.com', password: 'password' });
 
-  const users = await repo.getAll();
+  // const users = await repo.getAll();
 
-  console.log(users);
+  const user = await repo.getOne('4bac57e1');
+
+  console.log(user);
 };
 
 test();
