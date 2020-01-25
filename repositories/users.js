4128f789aaf93eed394e92434f45ffe42b03da57
Istvan Acs <istvan.acs89@gmail.com>
2020-01-25 14:07:40 +0000

    Opening the repo data file

--- repositories/users.js
+++ repositories/users.js
@@ -13,6 +13,25 @@ class UsersRepository {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
+  async getAll() {
+    // Open the file called this.filename
+    const contents = await fs.promises.readFile(this.filename, {
+      encoding: 'utf8'
+    });
+    // Parse the contents
+    const data = JSON.parse(contents);
+    // Return the parsed data
+    return data;
+  }
 }
 
-new UsersRepository('users.json');
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+
+  const users = await repo.getAll();
+
+  console.log(users);
+};
+
+test();
