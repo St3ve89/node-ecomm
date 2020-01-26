958dbabb140911ced828f79ffe632849f40a677c
Istvan Acs <istvan.acs89@gmail.com>
2020-01-26 14:09:17 +0000

    Fetching a session

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['ctvg423t456v']
+  })
+);
 
 app.get('/', (req, res) => {
   res.send(`
     <div>
+      Your id is: ${req.session.userId}
       <form method='POST'>
         <input name='email' placeholder="email"/>
         <input name='password' placeholder="password"/>
@@ -37,6 +44,7 @@ app.post('/', async (req, res) => {
   const user = await usersRepo.create({ email, password });
 
   // Store the id of that user inside the users cookie
+  req.session.userId = user.id; // req.session added by cookie-session
 
   res.send(`Account created!!! `);
 });
